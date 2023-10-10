2a9b5212af882881d01ec6381fb6b2f047121905
kevintruo <ktruo@kevintruo.com>
2023-10-10 01:36:58 -0400

    Updated Regulations

--- src/pages/Regulations/index.tsx
+++ src/pages/Regulations/index.tsx
@@ -2,67 +2,132 @@ import Section from "../../components/Section";
 
 export default function Regulations() {
   return (
-    <Section>
-      <div className='flex flex-row w-screen justify-between p-5'>
-        <div className='flex flex-col'>
-          <p className='text-3xl font-bold text-black  dark:text-white dark:bg-black'>
-            Club Regulations
-          </p>
-          <p>PLAYER CODE OF CONDUCT</p>
-          <p>
-            The Carlingwood Badminton Club will operate within a positive atmosphere of respect,
-            fair play and good sportsmanship. Regular board selection play will be offered to permit
-            challenging games at appropriate levels for all members, at the same time as providing
-            an opportunity for social play among all members
-          </p>
-          <ul>
+    <Section
+      className='overflow-auto mt-12 mb-3 gap-5 styled-scrollbar styled-scrollbar-track styled-scrollbar-thumb dark:styled-scrollbar-dark dark:styled-scrollbar-track-dark'
+      id='styled-scrollbar'>
+      <p className='text-3xl font-bold underline text-black  dark:text-white dark:bg-black'>
+        Club Regulations
+      </p>
+      <p className='text-xl text-blue-500'>PLAYER CODE OF CONDUCT</p>
+      <p>
+        The Carlingwood Badminton Club will operate within a positive atmosphere of respect, fair
+        play and good sportsmanship. Regular board selection play will be offered to permit
+        challenging games at appropriate levels for all members, at the same time as providing an
+        opportunity for social play among all members
+      </p>
+      <ul className='list-disc list-inside indent-3'>
+        <li>Respect the court time allowed for games. Leave your court promptly for rotation</li>
+        <li>
+          Be aware and respectful tog ames and players that are on other courts. For example:
+          players leaving or entering the court should od so as quickly and with as little
+          disruption as possible to the games that are in progress. Whenever possible, wait until
+          action on the courts that you have pass has stopped, or make your way to or from the court
+          through the hallway rather than through the gym
+        </li>
+        <li>Zero tolerance for loud, abusive or profane language.</li>
+        <li>Zero tolerance for racquet throwing or indiscriminately hitting the shuttle.</li>
+        <li>
+          Zero tolerance for harassment of any form, including but not limited to physical or
+          psychological abuse
+        </li>
+        <li>Keep positive whether you win or lose, whether you are having a good day or not.</li>
+        <li>
+          Develop a consistent habit of good sportsmanship and fair play, for example:
+          <ul className='list-decimal list-inside indent-10'>
+            <li>Keep the score correctly B state the score clearly after each rally</li>
             <li>
-              Respect the court time allowed for games. Leave your court promptly for rotation
+              Call the lines fairly (remember it is the lines on your own side of the net for which
+              you are responsible)
             </li>
-            <li>
-              Be aware and respectful tog ames and players that are on other courts. For example:
-              players leaving or entering the court should od so as quickly and with as little
-              disruption as possible to the games that are in progress. Whenever possible, wait
-              until action on the courts that you have pass has stopped, or make your way to or from
-              the court through the hallway rather than through the gym
-            </li>
-            <li>Zero tolerance for loud, abusive or profane language.</li>
-            <li>Zero tolerance for racquet throwing or indiscriminately hitting the shuttle.</li>
-            <li>
-              Zero tolerance for harassment of any form, including but not limited to physical or
-              psychological abuse
-            </li>
-            <li>
-              Keep positive whether you win or lose, whether you are having a good day or not.
-            </li>
-            <li>
-              <ul>
-                Develop a consistent habit of good sportsmanship and fair play, for example:
-                <li>Keep the score correctly B state the score clearly after each rally</li>
-                <li>
-                  Call the lines fairly (remember it is the lines on your own side of the net for
-                  which you are responsible)
-                </li>
-                <li>Shake hands after each match</li>
-                <li>Congratulate your opponent politely if you lose</li>
-                <li>
-                  Acknowledge a strength, good shot, or good rally of your opponent when you win.
-                </li>
-                <li>Accept congratulations graciously from your opponent.</li>
-              </ul>
-            </li>
-            <li>
-              Proper badminton attire required, example, indoor court shoes, shorts, and t-shirt.
-            </li>
-            <li>Chewing gum, candy not allowed while on court</li>
-            <li>Protective glasses strongly recommended</li>
-            <li>
-              Guests will be limited to allow maximum play time for members. Guest fees are $5.00
-              per session.
-            </li>
+            <li>Shake hands after each match</li>
+            <li>Congratulate your opponent politely if you lose</li>
+            <li>Acknowledge a strength, good shot, or good rally of your opponent when you win.</li>
+            <li>Accept congratulations graciously from your opponent.</li>
           </ul>
-        </div>
-      </div>
+        </li>
+        <li>Proper badminton attire required, example, indoor court shoes, shorts, and t-shirt.</li>
+        <li>Chewing gum, candy not allowed while on court</li>
+        <li>Protective glasses strongly recommended</li>
+        <li>
+          Guests will be limited to allow maximum play time for members. Guest fees are $5.00 per
+          session.
+        </li>
+      </ul>
+      <p className='text-xl text-blue-500'>CLUB REGULATIONS</p>
+      <ul className='list-disc list-inside indent-3'>
+        <li>
+          Members and any persons attending the Carlingwood Badminton Club shall ensure that they
+          comply with all rules and regulations posted in the facility and/or on the property, and
+          with all directions given by Confederation School and/or Woodroffe School representatives.
+        </li>
+        <li>
+          Carlingwood Badminton Club members and any persons attending the club shall only have
+          access to the: gymnasium, male & female change rooms & washrooms.
+        </li>
+        <li>
+          Carlingwood Badminton Club members and any persons attending the club must change into
+          appropriate athletic footwear and must not enter the gymnasium in shoes that have been
+          worn outside. The use of shoes marking the surfaces is strictly prohibited.
+        </li>
+        <li>
+          Smoking, alcoholic beverages, gambling or illicit substances are strictly prohibited
+          within the facility or otherwise within or on the property.
+        </li>
+        <li>
+          Any Member or guest attending the club will be asked to leave if he or she is intoxicated
+          and/or causing a disturbance.
+        </li>
+        <li>
+          Aisles, fire exits, lobbies, passageways, stairways or doorways are not to be blocked by
+          members or by anyone attending an event or activity.
+        </li>
+        <li>
+          Members agree not to use, store, or permit to be used or stored in or on any part of the
+          premises any flammable, combustible or explosive fluid, material, chemical or any
+          substance prohibited by any law or other policy.
+        </li>
+        <li>
+          Members shall not permit garbage or debris to accumulate at the facility or otherwise
+          within or about the premises. The schools provide waste receptacles in, or near, the
+          gymnasium.
+        </li>
+        <li>
+          Members shall be solely responsible for their own safety and the safety of their guests
+          attending the club. Protective eye-wear is strongly recommended for all players.
+        </li>
+        <li>
+          Members shall comply with all laws, orders and regulations of any level of government and
+          with any school policy or procedure.
+        </li>
+        <li>
+          Members shall not do or permit to be done any act which shall or might subject the school
+          and/or the Carlingwood Badminton Club to any liability or responsibility for injury to any
+          person or persons or to property by reason of such activity.
+        </li>
+        <li>
+          The Member shall indemnify the school and the Carlingwood Badminton Club against any loss,
+          damage, injury or death suffered from the use of the facility or the premises and for any
+          loss or damage to property of the school or the CBC caused by the negligence of the
+          member.
+        </li>
+        <li>
+          Carlingwood Badminton Club. The member assumes all responsibility of any damages caused by
+          the member and their guest(s) to the facility, property or equipment of either of the
+          schools or the Carlingwood Badminton Club. The member agrees to be responsible for the
+          cost of repair or replacement of any school or Carlingwood Badminton Club property lost,
+          stolen, damaged, destroyed or defaced by the member or their guest(s). Any such damage may
+          be repaired or replaced by the school and/or Carlingwood Badminton Club and all costs so
+          incurred by the school and/or the Carlingwood Badminton Club shall be payable by the
+          member on demand.
+        </li>
+        <li>
+          The school and the Carlingwood Badminton Club shall not be responsible for any loss or
+          damage, theft or destruction of the member's property by any cause of whatever nature.
+        </li>
+        <li>
+          At the end of each evening, the member agrees to leave the facility in a clean state.
+        </li>
+      </ul>
     </Section>
   );
 }
